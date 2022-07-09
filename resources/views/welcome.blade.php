ca9dfa799b154de8a62a836190e2baff1a89b2ef
Sahil Chahal <86303421+sahilchahal@users.noreply.github.com>
2022-07-10 02:49:33 +0530

    Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-    <div class="container">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi veniam praesentium neque amet quibusdam molestiae enim consequatur, nulla corporis eius earum. Sequi debitis provident explicabo impedit ipsam. Deleniti amet illo dolore expedita et alias, sequi recusandae minima ratione aspernatur nemo officiis unde temporibus reiciendis, asperiores deserunt incidunt obcaecati quibusdam iusto.</div>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+<div class="container">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi veniam praesentium neque amet quibusdam molestiae enim consequatur, nulla corporis eius earum. Sequi debitis provident explicabo impedit ipsam. Deleniti amet illo dolore expedita et alias, sequi recusandae minima ratione aspernatur nemo officiis unde temporibus reiciendis, asperiores deserunt incidunt obcaecati quibusdam iusto.
+
+
+</div>
+@endsection
