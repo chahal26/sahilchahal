ea7838399d1286c3115b8e9b4cbf35b15cfc22c7
Sahil Chahal <86303421+sahilchahal@users.noreply.github.com>
2022-07-10 22:41:08 +0530

    Category CRUD

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -24,4 +24,12 @@ class Category extends Model
     {
         return $this->belongsToMany(Blog::class);
     }
+
+    public function parent() {
+        return $this->hasOne(Category::class,'id','parent_id') ;
+    }
+
+    public function children() {
+        return $this->hasMany(Category::class,'parent_id','id');
+    }
 }
