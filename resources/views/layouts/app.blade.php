088c7dad9215c7299a42b1b9a50745bf3eaccbab
Sahil Chahal <86303421+sahilchahal@users.noreply.github.com>
2022-07-13 20:05:43 +0530

    Blogs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="chahal/img/favicon.png" type="image/png">
-        <title>{{ config('app.name', 'Sahil Chahal') }}</title>
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <title>{{ config('app.name', 'Code with Chahal') }}</title>
         {{-- Bootstrap --}}
         @vite(['resources/js/app.js'])
         <link rel="stylesheet" href="{{ asset('chahal/css/font-awesome.min.css') }}">
         <link rel="stylesheet" href="{{ asset('chahal/css/style.css') }}">
+        @livewireStyles
     </head>
 <body>
     <div id="app">
@@ -22,6 +23,7 @@
     {{-- Javascript --}}
 	<script src="{{ asset('chahal/js/jquery-3.2.1.min.js') }}"></script>
 	<script src="{{ asset('chahal/js/theme.js') }}"></script>
+    @livewireScripts
 </body>
 
 </html>
